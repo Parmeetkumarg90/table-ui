60cb89ea32f7af6402bc87fe21c9d0a0591a54ff
Parmeet Kumar <parmeetkumarg90@gmail.com>
2026-08-31 17:11:35 +0530

    Feature: rename project name

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: process.env.VERCEL ? undefined : "standalone",
+  reactCompiler: true,
 };
 
 export default nextConfig;
