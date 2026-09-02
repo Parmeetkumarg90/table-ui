3885eea6de5a4be0868455b318cdb12512f25b14
Parmeet Kumar <parmeetkumarg90@gmail.com>
2026-09-02 15:17:43 +0530

    Feature: integrate theme provider into AppLayoutComponent and add app theme configuration

--- frontend/src/features/layout/components/app-layout/app-layout.tsx
+++ frontend/src/features/layout/components/app-layout/app-layout.tsx
@@ -9,24 +9,28 @@ import { Provider } from "react-redux";
 import { SnackbarProvider } from "notistack";
 import { LeftSideBarComponent } from "@/features/app/components/left-sidebar/left-sidebar";
 import { NavbarComponent } from "@/features/app/components/nav-bar/nav-bar";
+import theme from "@/theme/app-theme";
+import { ThemeProvider } from "@mui/material/styles";
 
 const AppLayoutComponent = ({ children }: { children: ReactNode }) => {
   const [showSidebar, setShowSidebar] = useState<boolean>(false);
 
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persister}>
-        <SnackbarProvider maxSnack={3} autoHideDuration={2000}>
-          <NavbarComponent
-            onSideBarToggle={() => setShowSidebar((prev) => !prev)}
-          />
-          <Box className={styles.childrenLayout}>
-            {showSidebar && <LeftSideBarComponent />}
-            {children}
-          </Box>
-        </SnackbarProvider>
-      </PersistGate>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persister}>
+          <SnackbarProvider maxSnack={3} autoHideDuration={2000}>
+            <NavbarComponent
+              onSideBarToggle={() => setShowSidebar((prev) => !prev)}
+            />
+            <Box className={styles.childrenLayout}>
+              {showSidebar && <LeftSideBarComponent />}
+              {children}
+            </Box>
+          </SnackbarProvider>
+        </PersistGate>
+      </Provider>
+    </ThemeProvider>
   );
 };
 
